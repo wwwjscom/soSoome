c73e9c6ec78f444f9092f9865ba78dbdae5e9c26
Jason Soo <wwwjscom@gmail.com>
2010-04-04 01:15:23 +0100

    publications

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 	map.root :controller => "home"
 	map.about_me '/about', :controller => 'home', :action => 'about_me'
 	map.profile '/', :controller => nil
+	map.publications '/publications', :controller =>'home', :action => 'publications'
 	map.blog 'http://www.google.com', :controller => nil
 
   map.connect ':controller/:action/:id'
